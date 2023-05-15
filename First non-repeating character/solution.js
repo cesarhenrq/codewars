c559f7f478c0c668a8f4b1c5e2bf927545973342
César Henrique <cesaarhvb@gmail.com>
2023-05-15 13:02:12 -0300

    fixed solution

--- First non-repeating character/solution.js
+++ First non-repeating character/solution.js
@@ -4,7 +4,8 @@ const first_non_repeating_letter = str => {
   const firstWord = words.reduce((acc, currWord, currIndex) => {
     if (acc === '') {
       return words.some((word, index) => {
-        if (currIndex !== index) return word === currWord;
+        if (currIndex !== index)
+          return word.toLowerCase() === currWord.toLowerCase();
       })
         ? acc
         : currWord;
